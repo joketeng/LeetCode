ce865468627585bafbff06d61c9816c87bf9fbf0
joketeng <32324177+joketeng@users.noreply.github.com>
2018-10-12 20:38:36 +0800

    ZigZag Conversion

---
+++ 6.py
@@ -0,0 +1,14 @@
+class Solution():
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+        L = [''] * numRows
+        index, step = 0, 1
+        for i in s:
+            L[index] += i
+            if index == 0:
+                step = 1
+            elif index == numRows -1:
+                step = -1
+            index += step
+        return ''.join(L)
